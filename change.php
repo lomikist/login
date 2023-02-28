81f18a2d156a4a3f3dd153478fbca2104397cbc1
djweasle <armansarg01@gmail.com>
2023-02-28 12:36:43 +0400

    new changes

--- change.php
+++ change.php
@@ -4,71 +4,71 @@
  * Plugin Name: change_plugin
  * Description: this plugin i for saveing a date in date base 
  */
-$idFromGet = 0;
-$idFromDb = 0;
+$id_from_get = 0;
+$id_from_db = 0;
 
-//dsadas
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $idFromGet;
+    $id_from_get;
     foreach ($_GET as $key => $value) {
-        $idFromGet = $key;
+        $id_from_get = $key;
     }
-    // echo $id;
-    $a = $wpdb->get_results("SELECT * FROM wp_info WHERE id = $idFromGet");
-    foreach ($a as $key => $value) {
+
+    $row_id = $wpdb->get_results("SELECT * FROM wp_info WHERE id = $id_from_get");
+    foreach ($row_id as $key => $value) {
         print_r("Your current id is - ".$value->id." write it in input ");
-        $idFromDb = $value->id;
+        $row_id = $value->id;
     }
 };
-
-//sddssd
 
 if(isset($_POST['changeBtn']))
 {
     global $wpdb;
 
-    $arr = array(
-        'name1'=>$_POST['name'],
-        'surname'=>$_POST['surname'],
-        'email'=>$_POST['email']
-    );
-    $arrWhich = array(
-        'id'=>$_POST['id'],
-    );
+    $name = strip_tags($_POST['name']);
+    $surname = strip_tags($_POST['surname']);
+    $email = strip_tags($_POST['email']);
+    if (strlen($name) >= 3 && strlen($surname) >= 5 && strlen($email) >= 5) {
+        $arr = array(
+            'name1'=> $name,
+            'surname'=>$surname,
+            'email'=>$email
+        );
+        $arr_which = array(
+            'id'=>$_POST['id'],
+        );
 
-    print_r($arr);
-    print_r($arrWhich);
-    // $wpdb->update('wp_info',
-    //             $arr,
-    //             $arrWhich);
-    if($wpdb->update('wp_info',$arr,$arrWhich)){
-        echo "data updated";
+        if($wpdb->update('wp_info',$arr,$arr_which)){
+            echo "data updated";
+        }
+        else echo "error";
+    }else {
+        echo "name shold be more than 3 sibol , surname 5, email 5";
     }
-    else echo "error";
 };
 
+add_shortcode( 'edit_short', 'changing');
 
-// just comment 
-
-add_shortcode( 'change', 'changing');
 function changing(){    
-    global $idFromDb;
+    global $id_from_get;
 
-    return "
-    <form method = 'post' action='change.php'>
-        <label for='name'>new name </label>
-        <input type='text' name = 'name' id='name'><br>
-        
-        <label for='surname'>new surname</label>
-        <input type='text' name = 'surname' id='surname'><br>
-        
-        <label for='email'>new email</label>
-        <input type='text' name = 'email' id='email'><br>
+    ob_get_clean();
+    ob_start();
+    
+    echo "<form method = 'post' action='change.php'>
+            <label for='name'>new name </label>
+            <input type='text' name = 'name' id='name'><br>
+            
+            <label for='surname'>new surname</label>
+            <input type='text' name = 'surname' id='surname'><br>
+            
+            <label for='email'>new email</label>
+            <input type='text' name = 'email' id='email'><br>
 
-        <label for='id'>current ID</label>
-        <input type='number' name = 'id' id='id' value='$idFromDb' readonly='true'><br>
+            <label for='id'>current ID</label>
+            <input type='number' name = 'id' id='id' value='$id_from_get' readonly='true'><br>
+            
+            <input type='submit' name = 'changeBtn'>
+        </form>";
         
-        <input type='submit' name = 'changeBtn'>
-    </form>";
 }
--- my_test_plugin.php
+++ my_test_plugin.php
@@ -9,84 +9,93 @@ table, th, td {
  * Plugin Name: my_test_plugin
  * Description: this plugin i for saveing a date in date base 
  */
-$shortName = "myShort";
-add_shortcode($shortName , "showForm");
-//second
-function showForm()
+
+add_shortcode("log_form_short", "show_form");
+
+$log_url = site_url( "log_form_short" );
+
+function show_form()
 {
-    return '
-        <form method = "post" id = "forma" action="http://localhost/wordpress6/wordpress/myshort/">
-            <label for="name">Name</label>
-            <input name = "name" id = "name">
-            <br/>
-            <label for="surname">Surmane</label>
-            <input name = "surname" id = "surname">
-            <br/>
-            <label for="email">Email</label>
-            <input name = "email" id = "email">
-            <br/>
-            <label for="submitBtn"></label>
-            <input type = "submit" name = "submitBtn" id = "submitBtn">
-        </form>
-    ';
+    global $log_url;
+
+    ob_get_clean();
+    ob_start();
+    echo "<form method = 'post' id = 'forma' action='$log_url'>
+        <label for='name'>Name</label>   
+        <input name = 'name' id = 'name'>
+        <br/>    
+        <label for='surname'>Surmane</label>
+        <input name = 'surname' id = 'surname'>  
+        <br/>
+        <label for='email'>Email</label> 
+        <input name = 'email' id = 'email'>
+        <br/>    
+        <label for='submitBtn'></label>
+        <input type = 'submit' name = 'submitBtn' id = 'submitBtn'>
+        </form>";
 }
 
-//adsadssa
-function addDb()
+function add_db()
 {
     global $wpdb;
     if (isset($_POST['submitBtn'])) {
-        $name = $_POST['name'];
-        $surname = $_POST['surname'];
-        $email = $_POST['email'];
 
-        $tableName = $wpdb->prefix . 'info';
-        $data = array(
-            'name1' =>  $name,
-            'surname' => $surname,
-            'email' => $email
-        );
-        if($wpdb->insert($tableName, $data))
-        {
-            echo "data is added";
+        $name = strip_tags($_POST['name']);
+        $surname = strip_tags($_POST['surname']);
+        $email = strip_tags($_POST['email']);
+
+        if (strlen($name) > 3 && strlen($surname) > 5 && strlen($email) > 5) {
+            $tableName = $wpdb->prefix . 'info';
+            $data = array(
+                'name1' =>  $name,
+                'surname' => $surname,
+                'email' => $email
+            );
+            if($wpdb->insert($tableName, $data))
+            {
+                echo "data is added";
+            }
+        }else {
+            echo "name shold be more than 3 sibol , surname 5, email 5";
         }
     }
 }
 
-//adddddd
-add_action("acf/save_pot", "addDb");
+add_action("admin_post", "add_db");
 
-do_action("acf/save_pot");
+do_action("admin_post");
 
 
-add_shortcode("showTable" , "showTable");
+add_shortcode("show_table_short" , "show_table");
 
-// fsdf
-function showTable()
+function show_table()
 {
     global $wpdb;
-
-    $tableName = $wpdb->prefix . 'info';
+    $change_url = site_url( "change" );
     
-    $a = $wpdb->get_results("SELECT * FROM wp_info WHERE id < 100");
+    $arr_from_db = $wpdb->get_results("SELECT * FROM wp_info WHERE id < 100");
 
-    $tables = "<form action='http://localhost/wordpress6/wordpress/change/' method = 'get'>";
-    $tables .= "<table style = border: 1px solid black; >";
-    foreach ($a as $key) {
-        $tables .= "
-            <tr>
-                <td>$key->id</td>
-                <td>$key->name1</td>
-                <td>$key->surname</td>
-                <td>$key->email</td>
+    ob_get_clean();
+    ob_start();
+
+    echo"<form action='$change_url' method = 'get'>
+            <table style = border: 1px solid black;>";
+    foreach ($arr_from_db as $key) {
+        $real_id = esc_attr($key->id);
+        $real_name1 = esc_attr($key->name1);
+        $real_surname = esc_attr($key->surname);
+        $real_email = esc_attr($key->email);
+
+        echo "<tr>
+                <td>$real_id</td>
+                <td>$real_name1</td>
+                <td>$real_surname</td>
+                <td>$real_email</td>
                 <td>
-                    <input type='submit' value = 'edit' name = '$key->id'>
+                    <input type='submit' value = 'edit' name = '$real_id'>
                 </td>
-            </tr>
-        ";
+            </tr>";
     }
-    $tables .= "</table>";
-    $tables .= "</form>";
-
-    return $tables;
+    echo "</table>
+    </form>";
 }?>
