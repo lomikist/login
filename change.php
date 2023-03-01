34139e0864bd430cdee17e8a0798b5601c94275b
Arman Sargsyan <armansargsyan1@gmail.com>
2023-03-01 20:27:31 +0400

    needed replace echo with open close tags

--- change.php
+++ change.php
@@ -1,60 +1,48 @@
-
 <?php
 /**
  * Plugin Name: change_plugin
- * Description: this plugin i for saveing a date in date base 
+ * Description: this plugin i for saveing a date in date base
  */
-
-
-if(isset($_POST['changeBtn']))
-{
-    global $wpdb;
-
-    $name = sanitize_text_field($_POST['name']);
-    $surname = sanitize_text_field($_POST['surname']);
-    $email = sanitize_email($_POST['email']);
-    if (strlen($name) >= 3 && strlen($surname) >= 5 && strlen($email) >= 5) {
-        $arr = array(
-            'name1'=> $name,
-            'surname'=>$surname,
-            'email'=>$email
-        );
-        $arr_which = array(
-            'id'=>$_POST['id'],
-        );
-
-
-        if($wpdb->update($wpdb->prefix, $arr, $arr_which)){
-            echo "data updated";
-        }
-        else echo "error";
-    }else {
-        echo "name shold be more than 3 sibol , surname 5, email 5";
-    }
+if ( isset( $_POST['changeBtn'] ) ) {
+	global $wpdb;
+	$name    = sanitize_text_field( $_POST['name'] );
+	$surname = sanitize_text_field( $_POST['surname'] );
+	$email   = sanitize_email( $_POST['email'] );
+	if ( strlen( $name ) >= 3 && strlen( $surname ) >= 5 && strlen( $email ) >= 5 ) {
+		$arr       = array(
+			'name1'   => $name,
+			'surname' => $surname,
+			'email'   => $email
+		);
+		$arr_which = array(
+			'id' => $_POST['id'],
+		);
+		if ( $wpdb->update( $wpdb->prefix, $arr, $arr_which ) ) {
+			echo "data updated";
+		} else {
+			echo "error";
+		}
+	} else {
+		echo "name shold be more than 3 sibol , surname 5, email 5";
+	}
 };
 
-add_shortcode( 'edit_short', 'changing');
-
-function changing(){
+add_shortcode( 'edit_short', 'changing' );
+function changing() {
 	$id_from_get = 0;
-    
-	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	if ( $_SERVER["REQUEST_METHOD"] == "GET" ) {
 		global $wpdb;
-
-		foreach ($_GET as $key => $value) {
+		foreach ( $_GET as $key => $value ) {
 			$id_from_get = $key;
 		}
-
-		$row_id = $wpdb->get_results("SELECT * FROM wp_info WHERE id = $id_from_get");
-		foreach ($row_id as $key => $value) {
-			print_r("Your current id is - ".$value->id." write it in input ");
+		$row_id = $wpdb->get_results( "SELECT * FROM wp_info WHERE id = $id_from_get" );
+		foreach ( $row_id as $key => $value ) {
+			print_r( "Your current id is - " . $value->id . " write it in input " );
 			$row_id = $value->id;
 		}
 	};
-
-    ob_start(null, 0, PHP_OUTPUT_HANDLER_CLEANABLE ^ PHP_OUTPUT_HANDLER_REMOVABLE);
-
-    echo "<form method = 'post' action='change.php'>
+	ob_start( );
+	echo "<form method = 'post' action='change.php'>
             <label for='name'>new name </label>
             <input type='text' name = 'name' id='name'><br>
             
--- my_test_plugin.php
+++ my_test_plugin.php
@@ -1,25 +1,24 @@
-<style>
-table, th, td {
-    border:1px solid black;
-}
-</style>
+<!--<style>-->
+<!--table, th, td {-->
+<!--    border:1px solid black;-->
+<!--}-->
+<!--</style>-->
 
 <?php
 /**
  * Plugin Name: my_test_plugin
- * Description: this plugin i for saveing a date in date base 
+ * Description: this plugin is for saveing a date in date base
  */
 
-add_shortcode("log_form_short", "show_form");
+global $sum;
 
-function show_form()
-{
+add_shortcode( "log_form_short", "show_form" );
+function show_form() {
 	$log_url = site_url( "log_form_short" );
-
-    ob_start(null, 0, PHP_OUTPUT_HANDLER_CLEANABLE ^ PHP_OUTPUT_HANDLER_REMOVABLE);
-
-	echo "<form method = 'post' id = 'forma' action='$log_url'>
+	ob_start( );
+	echo "<form method = 'post' id = 'form' action='".admin_url( 'admin-post.php' )."'>
         <label for='name'>Name</label>   
+        <input type='hidden' name = 'action' value = 'submit_btn'>
         <input name = 'name' id = 'name'>
         <br/>    
         <label for='surname'>Surmane</label>
@@ -29,86 +28,130 @@ function show_form()
         <input name = 'email' id = 'email'>
         <br/>    
         <label for='submitBtn'></label>
-        <input type = 'submit' name = 'submitBtn' id = 'submitBtn'>
+        <input type = 'submit' id = 'submit_btn'>
         </form>";
 
 	return ob_get_clean();
 }
 
-function add_db()
-{
-    global $wpdb;
-    if (isset($_POST['submitBtn'])) {
-
-        $name = sanitize_text_field($_POST['name']);
-        $surname = sanitize_text_field($_POST['surname']);
-        $email = sanitize_text_field($_POST['email']);
-
-        $db_name = $wpdb->prefix;
-
-        if (strlen($name) > 3 && strlen($surname) > 5 && strlen($email) > 5) {
-            $data = array(
-                'name1' =>  $name,
-                'surname' => $surname,
-                'email' => $email
-            );
-            if($wpdb->insert($db_name, $data))
-            {
-                echo "data is added";
-            }else echo $wpdb->last_error;
-        }else {
-            echo "name shousld be more than 3 sibol , surname 5, email 5";
+function add_db() {
+	global $wpdb;
+    $name    = sanitize_text_field( $_POST['name'] );
+    $surname = sanitize_text_field( $_POST['surname'] );
+    $email   = sanitize_email( $_POST['email'] );
+    $db_name = $wpdb->prefix;
+    if ( strlen( $name ) > 3 && strlen( $surname ) > 5 && strlen( $email ) > 5 ) {
+        $data = array(
+            'name1'   => $name,
+            'surname' => $surname,
+            'email'   => $email
+        );
+        if ( $wpdb->insert( $db_name, $data ) ) {
+            echo "data is added";
+        } else {
+            echo $wpdb->last_error;
         }
+    } else {
+        echo "name shousld be more than 3 sibol , surname 5, email 5";
     }
 }
+add_action( "admin_post_submit_btn", "add_db" );
 
+add_shortcode( "show_table_short", "show_table" );
+function show_table() {
+	global $wpdb;
+	$last_id = 0;
 
-add_action("admin_post", "add_db");
+	$change_url  = site_url( "cahnge_short" );
 
-do_action("admin_post");
+	$arr_from_db = $wpdb->get_results( "SELECT * FROM wp_info WHERE id < 100" );
 
-add_shortcode("show_table_short" , "show_table");
+	ob_start();
 
-function show_table()
-{
-    global $wpdb;
-    $change_url = site_url( "cahnge_short" );
-    $arr_from_db = $wpdb->get_results("SELECT * FROM wp_info WHERE id < 100");
-
-	ob_start(null, 0, PHP_OUTPUT_HANDLER_CLEANABLE ^ PHP_OUTPUT_HANDLER_REMOVABLE);
-
-    echo"<form action='$change_url' method = 'get'>
+	echo "
+            <form action='$change_url' method = 'get'>
             <table style = border: 1px solid black;>";
-    foreach ($arr_from_db as $key) {
-        $real_id = esc_attr($key->id);
-        $real_name1 = esc_attr($key->name1);
-        $real_surname = esc_attr($key->surname);
-        $real_email = esc_attr($key->email);
 
-    echo "<tr>
-            <td>$real_id</td>
-            <td>$real_name1</td>
-            <td>$real_surname</td>
-            <td>$real_email</td>
-            <td>
-                <input type='submit' value = 'edit' name = '$real_id'>
-            </td>
-        </tr>";
-    }
-    echo "<input type='submit' value = 'next' name = 'next'>
+	if ( count( $arr_from_db ) >= 10 ) {
+		for ( $i = 0; $i < 10; $i ++ ) {
+            $real_id      = esc_attr( $arr_from_db[ $i ]->id );
+			$real_name1   = esc_attr( $arr_from_db[ $i ]->name1 );
+			$real_surname = esc_attr( $arr_from_db[ $i ]->surname );
+			$real_email   = esc_attr( $arr_from_db[ $i ]->email );
+			echo "<tr>
+                    <td>$real_id</td>
+                    <td>$real_name1</td>
+                    <td>$real_surname</td>
+                    <td>$real_email</td>
+                    <td>
+                        <input type='submit' value = 'edit' name = '$real_id'>
+                    </td>
+                </tr>";
+            $last_id = $real_id;
+		}
+	} else {
+		for ( $i = 0; $i < count( $arr_from_db ); $i ++ ) {
+			$real_id      = esc_attr( $arr_from_db[ $i ]->id );
+			$real_name1   = esc_attr( $arr_from_db[ $i ]->name1 );
+			$real_surname = esc_attr( $arr_from_db[ $i ]->surname );
+			$real_email   = esc_attr( $arr_from_db[ $i ]->email );
+			echo "<tr>
+                    <td>$real_id</td>
+                    <td>$real_name1</td>
+                    <td>$real_surname</td>
+                    <td>$real_email</td>
+                    <td>
+                        <input type='submit' value = 'edit' name = '$real_id'>
+                    </td>
+                </tr>";
+		}
+	}
+	echo "<input type='hidden' name = 'last_id' value = '$last_id'>
         </table>
-    </form>";
-    
+    </form>
+     <form action=".admin_url( 'admin-post.php' )." method = 'get' >
+        <input type='hidden' name = 'action' value = 'next_pg'>
+        <input type='submit' value='next'>
+      </form>";
 	return ob_get_clean();
 }
 
-function show_next()
-{
-    echo "next";
+
+function show_next() {
+	global $wpdb;
+    $last_id = 0;
+	$arr_from_db = $wpdb->get_results( "SELECT * FROM wp_info WHERE id > '".$_GET['last_id']."'" );
+
+	echo "<form action='".site_url( "cahnge_short" )."' method = 'get'>
+            <table style = border: 1px solid black;>";
+
+        for ( $i = 0; $i < 10; $i ++ ) {
+            $real_id      = esc_attr( $arr_from_db[ $i ]->id );
+            $real_name1   = esc_attr( $arr_from_db[ $i ]->name1 );
+            $real_surname = esc_attr( $arr_from_db[ $i ]->surname );
+            $real_email   = esc_attr( $arr_from_db[ $i ]->email );
+            $last_id = $real_id;
+            echo "<tr>
+                        <td>$real_id</td>
+                        <td>$real_name1</td>
+                        <td>$real_surname</td>
+                        <td>$real_email</td>
+                        <td>
+                            <input type='submit' value = 'edit' name = '$real_id'>
+                        </td>
+                    </tr>";
+        }
+
+	echo    "
+        </table>
+    </form>
+    <form action=".admin_url( 'admin-post.php' )." method = 'get' >
+        <input type='hidden' name = 'action' value = 'next_pg'>
+        <input type='hidden' name = 'last_id' value = '$last_id'>
+        <input type='submit' value = 'next'>
+    </form>";
 }
 
-add_action("admin_post_{$_GET['next']}", "show_next");
-
-do_action("admin_post_{$_GET['next']}");
+add_action( "admin_post_next_pg", 'show_next' );
 
 ?>
