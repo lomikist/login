7b27b405208272ca3de983e75f1662d206257c4d
Arman Sargsyan <armansargsyan1@gmail.com>
2023-03-08 18:37:58 +0400

    fixing: some bugs

--- my_test_plugin.php
+++ my_test_plugin.php
@@ -50,18 +50,16 @@ function add_db() {
 	} else {
 		echo "name shousld be more than 3 sibol , surname 5, email 5";
 	}
-    if (count($_FILES) > 0) {
-	    $lastid = $wpdb->insert_id; //above mentioned, user id
-	    file_upload( $lastid );
-    };
+	if ( count( $_FILES ) > 0 ) {
+		$lastid = $wpdb->insert_id; //above mentioned, user id
+		file_upload( $lastid );
+	};
 }
 
 add_action( "admin_post_submit_btn", "add_db" );
-
-
 // this function is adding img in (wp media) and adding img_src in db
 function file_upload( $id ) {
-    print_r(1234);
+	print_r( 1234 );
 	global $wpdb;
 	$upload = wp_upload_bits( $_FILES['image']['name'], null, file_get_contents( $_FILES['image']['tmp_name'] ) );
 	if ( ! $upload['error'] ) {
@@ -99,94 +97,69 @@ function file_upload( $id ) {
 
 function show_table() {
 	global $wpdb;
-	if ( isset($_GET['last_id'])) {
-		$last_id = $_GET['last_id'];
+	if ( isset( $_GET['last_id'] ) ) {
+		$last_id          = $_GET['last_id'];
 		$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info` WHERE id <= %d", $last_id );
 		$arr_from_db      = $wpdb->get_results( $show_users_query );
-        $arr_from_db      = array_slice($arr_from_db, count($arr_from_db) - 5, count($arr_from_db));
-	}else if ( isset($_GET['first_id'])) {
-		$first_id = $_GET['first_id'];
-		$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info` WHERE id >= %d", $first_id );
+		$arr_from_db      = array_slice( $arr_from_db, count( $arr_from_db ) - 5, count( $arr_from_db ) );
+	} elseif ( isset( $_GET['first_id'] ) ) {
+		$first_id         = $_GET['first_id'];
+		$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info` WHERE id > %d", $first_id );
 		$arr_from_db      = $wpdb->get_results( $show_users_query );
-	}else {
-		$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info`");
+	} else {
+		$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info`" );
 		$arr_from_db      = $wpdb->get_results( $show_users_query );
-		$last_id = 0;
+		$last_id          = 0;
 	}
-    ob_start();
+	ob_start();
 	?>
-    <form method="get" action="<?php echo admin_url( 'admin-post.php' ) ?>">
-        <input type="hidden" name="action" value="edit_short">
-        <table class="table table-striped table-dark" >
+    <form method="get" action="<?php echo site_url( 'edit' ) ?>">
+        <input type="hidden" name="action" value="edit_func">
+        <table class="table table-striped table-dark">
 			<?php
-				for ( $i = 0; $i < 5; $i ++ ) {
-					?>
-                    <tr>
-                        <td ><?php echo esc_attr( $arr_from_db[ $i ]->id ) ?></td>
-                        <td ><?php echo esc_attr( $arr_from_db[ $i ]->name1 ) ?></td>
-                        <td ><?php echo esc_attr( $arr_from_db[ $i ]->surname ) ?></td>
-                        <td ><?php echo esc_attr( $arr_from_db[ $i ]->email ) ?></td>
-                        <td >
-                            <input type="submit" name="edit" value="<?php echo esc_attr( $arr_from_db[ $i ]->id ) ?>">
-                        </td>
-                        <td>
-                            <img src="<?php echo esc_attr( $arr_from_db[ $i ]->img_src) ?>" alt="" width="50" height="50" sizes="" srcset="">
-                        </td>
-                    </tr>
-					<?php
-					$last_id = esc_attr( $arr_from_db[ $i ]->id );
-			    }
-            ?>
-            <input type="hidden" name="last_id" value="<?php echo $last_id ?>">
+			for ( $i = 0; $i < 5; $i ++ ) {
+				?>
+                <tr>
+                    <td><?php echo esc_attr( $arr_from_db[ $i ]->id ) ?></td>
+                    <td><?php echo esc_attr( $arr_from_db[ $i ]->name1 ) ?></td>
+                    <td><?php echo esc_attr( $arr_from_db[ $i ]->surname ) ?></td>
+                    <td><?php echo esc_attr( $arr_from_db[ $i ]->email ) ?></td>
+                    <td>
+                        <input type="submit" name="edit" value="<?php echo esc_attr( $arr_from_db[ $i ]->id ) ?>">
+                    </td>
+                    <td>
+                        <img src="<?php echo esc_attr( $arr_from_db[ $i ]->img_src ) ?>" alt="" width="50" height="50" sizes="" srcset="">
+                    </td>
+                </tr>
+				<?php
+				$last_id = esc_attr( $arr_from_db[ $i ]->id );
+			}
+			?>
         </table>
     </form>
 
-    <form action="<?php echo site_url('/users/')?>" method="get">
+    <form action="<?php echo site_url( '/users/' ) ?>" method="get">
         <input type="hidden" name="action" value="next_pg">
-        <input type="hidden" name="last_id" value="<?php echo $last_id + 5?>">
+        <input type="hidden" name="last_id" value="<?php echo $last_id + 5 ?>">
         <input type="submit" value="next">
     </form>
-    <form action="<?php echo site_url('/users/')?>" method="get">
+    <form action="<?php echo site_url( '/users/' ) ?>" method="get">
         <input type="hidden" name="action" value="prev_pg">
-        <input type="hidden" name="first_id" value="<?php echo $last_id - 10?>">
+        <input type="hidden" name="first_id" value="<?php echo $last_id - 10 ?>">
         <input type="submit" value="prev">
     </form>
 	<?php
 	return ob_get_clean();
 }//show_table func bracket
 add_shortcode( "mtp_show_users", "show_table" );
-
-function show_next() {
-	echo "show_next";
-	global $wpdb;
-	$last_id = 0;
-	// print_r($_GET);
-	$show_users_query = $wpdb->prepare( "SELECT * FROM `wp_info` WHERE id > %d", $_GET['last_id'] );
-	if ( $_GET['last_id'] == '' ) {
-		$first_id    = (string) ( (int) $_GET['last_id'] + 1 );//cuz last id is the previus previus page last_id
-		$arr_from_db = $wpdb->get_results( $show_users_query );
-	} else {
-		$first_id    = $_GET['last_id'];
-		$arr_from_db = $wpdb->get_results( $show_users_query );
-	}
-}
-add_action( "admin_post_next_pg", 'show_next' );
-
-//function show_prev() {
-//	echo "show_prev";
-//	global $wpdb;
-//	$last_id          = (int) $_GET['first_id'];
-//	$show_users_query = $wpdb->prepare( "SELECT * FROM wp_info WHERE id <= %d", $last_id );
-//	$arr_from_db      = $wpdb->get_results( $show_users_query );
-//	$arr_for_show     = array_slice( $arr_from_db, count( $arr_from_db ) - 5, count( $arr_from_db ) );
-//}// show_prev func brackets
-//add_action( 'admin_post_prev_pg', 'show_prev' );
-
-function edit_short() {
+function edit_func() {
 	$id_from_get = 0;
-	$id_from_get = $_GET['edit'];
+	if ( isset( $_GET['edit'] ) ) {
+		$id_from_get = $_GET['edit'];
+	}
 	ob_start(); ?>
-    <form method="get" action="<?php echo admin_url( 'admin-post.php' ) ?>">
+    <form method="get" action="<?php echo site_url( '/' ) ?>">
+
         <input type="hidden" name="action" value="edit_users">
 
         <label for="name">new name </label>
@@ -206,8 +179,8 @@ function edit_short() {
 	<?php
 	echo ob_get_clean();
 }
-add_action( 'admin_post_edit_short', 'edit_short' );
 
+add_shortcode( "mtp_edit_users", "edit_func" );
 function edit_users() {
 	global $wpdb;
 	$name    = sanitize_text_field( $_GET['name'] );
@@ -237,17 +210,16 @@ function edit_users() {
     </form>
 	<?php
 }
-add_action( "admin_post_edit_users", "edit_users" );
 
+add_action( "admin_post_edit_users", "edit_users" );
 // JS part --------------------------------------------------------------------
 add_action( 'wp_enqueue_scripts', 'js_script' );
-
 function js_script() {
 	wp_enqueue_script( 'custom_script', plugin_dir_url( __FILE__ ) . '/my_test_plugin.js', [ 'jquery' ] );
 	wp_localize_script( 'custom_script', 'MYSCRIPT', array(
 		'ajaxUrl' => admin_url( 'admin-ajax.php' ),
 	) );
-    wp_enqueue_style('style', plugin_dir_url( __FILE__ ).'my_test_plugin.css');
+	wp_enqueue_style( 'style', plugin_dir_url( __FILE__ ) . 'my_test_plugin.css' );
 	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' );
 
 }
@@ -259,14 +231,14 @@ function mtp_js_user_registration() {
 	?>
     <form id="form" enctype="multipart/form-data">
         <label for="name">Name</label>
-        <input name="name" id="name"  class="form-control">
+        <input name="name" id="name" class="form-control">
         <label for="surname">Surmane</label>
-        <input name="surname" id="surname"  class="form-control">
+        <input name="surname" id="surname" class="form-control">
         <label for="email">Email</label>
-        <input name="email" id="email"  class="form-control" >
-<!--        <label for="upload_img">upload img</label>-->
-<!--        <input type="file" id="file">-->
-<!--        <br/>-->
+        <input name="email" id="email" class="form-control">
+        <!--        <label for="upload_img">upload img</label>-->
+        <!--        <input type="file" id="file">-->
+        <!--        <br/>-->
         <label for="submitBtn"></label>
         <button name="upload_file" id="js_submit_btn" class="btn btn-primary"> register</button>
     </form>
@@ -276,8 +248,8 @@ function mtp_js_user_registration() {
 
 add_action( 'wp_ajax_my_ajax_request', 'adding_with_js' );
 add_action( 'wp_ajax_nopriv_my_ajax_request', 'adding_with_js' );
-
 function adding_with_js() {
 	add_db();
 }
+
 ?>
